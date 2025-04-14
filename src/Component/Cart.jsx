f386cf8a3d5ab6354817bf1fb4397accefd523d1
Apurwa <apurwatiwari13@gmail.com>
2025-04-14 15:51:59 +0530

    third

--- src/Component/Cart.jsx
+++ src/Component/Cart.jsx
@@ -2,30 +2,33 @@ import React from 'react'
 
 const Cart = ({ items, updateQuantity }) => {
     return (
-      <div className="cart">
-        {items.length === 0 && <p>No items added.</p>}
-        {items.map(item => (
-          <div className="cart-item" key={item.id}>
-            <div>
-           <div>
-           <span>{item.name}</span>
-           </div>
-            <div>
-            <span>₹{item.price} x {item.quantity}</span>
+        <>
+            <h3 className='h3-cart'>Cart Items</h3>
+            <div className="cart">
+                {items.length === 0 && <div className='empty'><span>Your cart is empty.</span><span> Add some product to see them here.</span></div>}
+                {items.map(item => (
+                <div className="cart-item" key={item.id}>
+                    <div>
+                <div>
+                <span>{item.name}</span>
+                </div>
+                    <div>
+                    <span>₹{item.price} x {item.quantity}</span>
+                    </div>
+                    </div>
+                    <div className='add-minus'>
+                    {!item.isGift && (
+                        <>
+                        <button className="minus" onClick={() => updateQuantity(item.id, item.quantity - 1)}>-</button>
+                        <span>{item.quantity}</span>
+                        <button className="plus" onClick={() => updateQuantity(item.id, item.quantity + 1)}>+</button>
+                        </>
+                    )}
+                    </div>
+                </div>
+                ))}
             </div>
-            </div>
-            <div className='add-minus'>
-              {!item.isGift && (
-                <>
-                  <button className="minus" onClick={() => updateQuantity(item.id, item.quantity - 1)}>-</button>
-                  <span>{item.quantity}</span>
-                  <button className="plus" onClick={() => updateQuantity(item.id, item.quantity + 1)}>+</button>
-                </>
-              )}
-            </div>
-          </div>
-        ))}
-      </div>
+        </>
     );
   };
   
